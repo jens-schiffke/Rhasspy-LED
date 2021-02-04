a035f10e08555847c63bc22e3bf1c75bdd73f22b
jens-schiffke <56131792+jens-schiffke@users.noreply.github.com>
2021-02-04 18:57:16 +0100

    erweitert auf 3 LEDs

--- Rhasspy-LED/Rhasspy-LED.py
+++ Rhasspy-LED/Rhasspy-LED.py
@@ -29,7 +29,7 @@ with open(rhasspyConfig,'r', encoding='UTF-8') as file:
     else:
       MQTTport = 1883
 
-strip = apa102.APA102(num_led=4)
+strip = apa102.APA102(num_led=3)
 strip.clear_strip()
 
 def on_connect(client, userdata, flags, rc):
@@ -40,13 +40,16 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     jsonData = json.loads(msg.payload)
     if msg.topic == "hermes/hotword/toggleOff" and "dialogueSession" in str(msg.payload) and jsonData["siteId"] == siteId and LED == "on":
-      strip.set_pixel(1,0,255,0,7)
+      for i in range(0,3):
+        strip.set_pixel(i,0,127,0,7)
       strip.show()
     elif msg.topic == "hermes/dialogueManager/sessionEnded" and jsonData["siteId"] == siteId:
-      strip.set_pixel(1,0,255,0,0)
+      for i in range(0,3):
+        strip.set_pixel(i,0,127,0,0)
       strip.show()
     elif jsonData["siteId"] == siteId and jsonData["reason"] == "ttsSay" and LED == "on":
-      strip.set_pixel(1,0,0,255,7)
+      for i in range(0,3):
+        strip.set_pixel(i,0,0,127,7)
       strip.show()
 
 client = mqtt.Client()
